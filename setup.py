77339d321e6d5c216535c583f958e71bac7a6bc8
Hiroyoshi <torotoki@users.noreply.github.com>
2017-09-10 15:07:15 +0900

    fixed the directory structure

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
 
-if __name__ == '__main__':
-  setup(
-    name='lm',
-    version='0.0.1',
-    description='I/O management tools for experiments in Machine Learning',
-    author='Hiroyoshi Komatsu',
-    author_email='hiroyoshi.komat@gmail.com',
-    url='https://github.com/torotoki/lm',
-    classifiers=[],
-    packages=find_packages(),
-    install_requires=[],
-    tests_require=[],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points="""
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name='lm',
+  version='0.0.1',
+  description='I/O management tools for experiments in Machine Learning',
+  license = 'GPL-3.0',
+  keywords = 'machine-learning experiments loggin python',
+  author='Hiroyoshi Komatsu',
+  author_email='hiroyoshi.komat@gmail.com',
+  url='https://github.com/torotoki/lm',
+  classifiers=[],
+  packages=['lm'],
+  long_description=read('README.md'),
+  entry_points="""
     [console_scripts]
     lm = lm.lm:main
-    """,
-  )
+  """,
+)
 
 
